41926b3f0f48f62a8490347bc6b036bb6a9fb0a7
vladislavslobodanuk <vladslobodanuk9@gmail.com>
2023-06-09 11:41:03 +0300

    Deleted folder idea from git

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,7 @@ export default function Home() {
     <div className="bg-blue-900 w-screen h-screen flex items-center">
       <div className='w-full text-center'>
         <h1 className="text-3xl text-white">
-          Init project
+          Init project!
         </h1>
       </div>
     </div>
